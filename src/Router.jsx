513e7ed016a338aea76fab9e08bced87a523c3f9
gaidenshinji <gaidenshinji2468@gmail.com>
2023-01-26 16:08:03 -0400

    Configuraciones iniciales listas

---
+++ src/Router.jsx
@@ -0,0 +1,17 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+
+function Router()
+{
+    return (
+        <BrowserRouter>
+	    <Routes>
+	    </Routes>
+	</BrowserRouter>
+    );
+}
+
+export default Router;
--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {Provider} from "react-redux";
+import store from "./store";
+
+import Router from "./Router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-    
+        <Provider store={store}>
+	    <Router/>
+	</Provider>
     </React.StrictMode>
 );
