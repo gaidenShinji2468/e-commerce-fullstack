7d2933671ddf3cfe4014e83b57554cd596230780
ftorrelles <torrellesf93@gmai.com>
2023-01-30 17:05:19 -0400

    primeros cambios en purchases

---
+++ src/components/ModalPurchases.jsx
@@ -0,0 +1,62 @@
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Table from "react-bootstrap/Table";
+
+const ModalPurchases = ({ show, handleClose }) => {
+    return (
+        <div>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header
+                    style={{
+                        display: "flex",
+                        flexDirection: "column",
+                        alignItems: "flex-start",
+                    }}
+                >
+                    <Modal.Title>Electronic bill</Modal.Title>
+                    <Modal.Body>
+                        <p>friend shop c.a </p> J‑29989842‑2
+                    </Modal.Body>
+                </Modal.Header>
+                <Modal.Body>
+                    <Table responsive>
+                        <thead>
+                            <tr>
+                                <th>product</th>
+                                <th>price</th>
+                                <th>amount</th>
+                                <th>Total to pay</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Samsung Galaxy S22</td>
+                                <td>1399.00</td>
+                                <td>1</td>
+                                <td>1399.00</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Modal.Body>
+                <Modal.Body
+                    style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                    }}
+                >
+                    <p>
+                        <strong>total paid</strong>
+                    </p>
+                    <p>1399.00</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+};
+
+export default ModalPurchases;
---
+++ src/pages/Purchases.jsx
@@ -0,0 +1,45 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { useState } from "react";
+import ModalPurchases from "../components/ModalPurchases";
+
+const Purchases = () => {
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    return (
+        <div>
+            <h2>My purchases</h2>
+            <hr />
+
+            <Card>
+                <Card.Header>purchase</Card.Header>
+
+                <Card.Body
+                    style={{ display: "flex", justifyContent: "space-around" }}
+                >
+                    <Card.Text>Samsung Galaxy S22</Card.Text>
+                    <Card.Text
+                        style={{
+                            width: "30px",
+                            height: "30px",
+                            border: "1px solid rgb(0, 0, 0)",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                        }}
+                    >
+                        1
+                    </Card.Text>
+                    <Card.Text>1399.00</Card.Text>
+                    <Button variant="primary" onClick={handleShow}>
+                        see details
+                    </Button>
+                </Card.Body>
+            </Card>
+            <ModalPurchases show={show} handleClose={handleClose} />
+        </div>
+    );
+};
+
+export default Purchases;
